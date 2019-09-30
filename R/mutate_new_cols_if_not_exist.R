8dbf77063426ddbc0251d71c2ccdc440adaeb514
patelm9 <patelm9@mskcc.org>
2019-09-30 15:34:57 -0400

    +: mutate_new_cols_if_not_exist function

---
+++ R/mutate_new_cols_if_not_exist.R
@@ -0,0 +1,23 @@
+#' Adds a column if it doesn't exist in the dataframe
+#' @param column_name character string of new column name
+#' @param value character vector of length 1 of the new value. Defaults to "".
+#' @importFrom magrittr %>%
+#' @importFrom dplyr mutate
+#' @export
+
+mutate_new_cols_if_not_exist <-
+        function(dataframe, column_name, value = "") {
+                if (!(column_name %in% colnames(dataframe))) {
+                        
+                        df_name <- deparse(substitute(dataframe))
+                        
+                        column_name <- enquo(column_name)
+                        
+                        x <-
+                                dataframe %>%
+                                dplyr::mutate(!!column_name := "")
+                        
+                        assign(df_name, x, envir = globalenv())
+                }
+                
+        }
