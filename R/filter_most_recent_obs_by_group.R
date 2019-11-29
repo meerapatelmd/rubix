ffd7538356eca532c72ca484524bdb15d6448637
patelm9 <patelm9@mskcc.org>
2019-11-28 21:27:42 -0500

    +: date only feature to filter_most_recent_obs function

--- R/filter_most_recent_obs_by_group.R
+++ R/filter_most_recent_obs_by_group.R
@@ -1,23 +1,41 @@
-#' Filters most recent values in dataframe
-#' @param group_by_col grouping variable
-#' @param index_date_col variable that will be used to filter by time
+#' Filters most recent values in dataframe based on timestamp or date data
+#' @param ... grouping variables
+#' @param index_time_col variable that will be used to filter by time or datettime
 #' @importFrom dplyr group_by
 #' @importFrom dplyr mutate_at
 #' @importFrom dplyr arrange
 #' @importFrom dplyr filter
 #' @importFrom lubridate ymd_hms
+#' @importFrom lubridate ymd
 #' @export
 
 filter_most_recent_obs_by_group <-
-        function (dataframe, group_by_col, index_date_col) {
-                group_by_col   <- enquo(group_by_col)
-                index_date_col <- enquo(index_date_col)
+        function (dataframe, ..., index_time_col, time_format = c("date", "timestamp")) {
+                group_by_cols   <- enquos(...)
+                index_time_col <- enquo(index_time_col)
 
-                dataframe %>%
-                        dplyr::ungroup() %>%
-                        dplyr::group_by(!!group_by_col) %>%
-                        dplyr::mutate_at(vars(!!index_date_col), funs(lubridate::ymd_hms(.))) %>%
-                        dplyr::arrange(desc(!!index_date_col)) %>%
-                        dplyr::filter(row_number() == 1) %>%
-                        dplyr::ungroup()
+                if (time_format == "timestamp") {
+                        return(
+                        dataframe %>%
+                                dplyr::ungroup() %>%
+                                dplyr::group_by(!!!group_by_cols) %>%
+                                dplyr::mutate_at(vars(!!index_time_col), funs(lubridate::ymd_hms(.))) %>%
+                                dplyr::arrange(desc(!!index_time_col)) %>%
+                                dplyr::filter(row_number() == 1) %>%
+                                dplyr::ungroup() %>%
+                                dplyr::mutate_all(as.character)
+                        )
+                } else if (time_format == "date") {
+                        return(
+                                dataframe %>%
+                                        dplyr::ungroup() %>%
+                                        dplyr::group_by(!!!group_by_cols) %>%
+                                        dplyr::mutate_at(vars(!!index_time_col), funs(lubridate::ymd(.))) %>%
+                                        dplyr::arrange(desc(!!index_time_col)) %>%
+                                        dplyr::filter(row_number() == 1) %>%
+                                        dplyr::ungroup() %>%
+                                        dplyr::mutate_all(as.character)
+                        )
+                }
+
         }
