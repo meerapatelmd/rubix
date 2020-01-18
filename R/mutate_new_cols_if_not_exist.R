24688ea8ed07b71cc25b7248ef0b7e476e12f0a8
Meera Patel <patelmeeray@gmail.com>
2020-01-18 13:08:22 -0500

    bug fix argument for function
    value argument was not being included as the contents of new column

--- R/mutate_new_cols_if_not_exist.R
+++ R/mutate_new_cols_if_not_exist.R
@@ -8,14 +8,11 @@
 mutate_new_cols_if_not_exist <-
         function(dataframe, column_name, value = "") {
                 if (!(column_name %in% colnames(dataframe))) {
-
-                        df_name <- deparse(substitute(dataframe))
-
                         column_name <- enquo(column_name)
 
                         x <-
                                 dataframe %>%
-                                dplyr::mutate(!!column_name := "")
+                                dplyr::mutate(`:=`(!!column_name, value))
 
                         return(x)
                 } else {
