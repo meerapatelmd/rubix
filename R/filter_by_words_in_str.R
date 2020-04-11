5799562a68229733cb512076204a7154fbbe54bc
Meera <patelm9@mskcc.org>
2020-04-10 21:13:56 -0400

    add function that filters by looping over words in a string

---
+++ R/filter_by_words_in_str.R
@@ -0,0 +1,22 @@
+#' Loop over searching for words in a string in a dataframe
+#' @importFrom secretary typewrite
+#' @export
+
+filter_by_words_in_str <-
+        function(string, split, dataframe, col) {
+                Args <- strsplit(string, split = split) %>% unlist()
+                
+                while (length(Args) > 0) {
+                        if (nrow(dataframe) > 0) {
+                                dataframe <-
+                                        dataframe %>%
+                                        filter_at_grepl(col = col, Args[1])
+                                
+                                Args <- Args[-1]
+                        } else {
+                                secretary::typewrite("Stopped at ", paste(Args, collapse = " "))
+                                return(dataframe)
+                        }
+                }
+                return(dataframe)
+        }
