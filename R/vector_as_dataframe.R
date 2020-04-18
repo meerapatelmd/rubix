e211906706932602b702fba0234ee2f96b1a7144
Meera <patelm9@mskcc.org>
2020-04-18 00:49:40 -0400

    testing out example feature

--- R/vector_as_dataframe.R
+++ R/vector_as_dataframe.R
@@ -1,13 +1,14 @@
-#' Converts vector to a dataframe with names as column names
+#' Convert a vector to a dataframe
+#' This function takes a vector and 
 #' @param vector vector of any length with names
 #' @importFrom dplyr tibble
 #' @return dataframe
-#' @examples 
-#' x <- c("a", "b", "c")
-#' vector_as_dataframe(x) 
+#' @example 
+x <- c("a", "b", "c")
+vector_as_dataframe(x) 
 #' @export
 
 vector_as_dataframe <-
         function(vector) {
-                return(dplyr::tibble(t(vector)))
+                return(as.data.frame(t(vector)))
         }
