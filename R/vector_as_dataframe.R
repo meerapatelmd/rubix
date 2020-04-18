0ab03426a953c97f4719519ab20e85584fee059f
Meera <patelm9@mskcc.org>
2020-04-18 00:55:12 -0400

    testing out example feature

--- R/vector_as_dataframe.R
+++ R/vector_as_dataframe.R
@@ -1,5 +1,5 @@
 #' Convert a vector to a dataframe
-#' This function takes a vector and 
+#' This function takes a vector and returns a dataframe with one row of values provided by the vector. Column names are those of the vector or the generic names in cases of unnamed vectors.
 #' @param vector vector of any length with names
 #' @importFrom dplyr tibble
 #' @return dataframe
