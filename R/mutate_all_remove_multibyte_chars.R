eb7581d28d8e6bac9bbdccfd5918dc041b8d4363
Meera <patelm9@mskcc.org>
2020-04-27 16:23:52 -0400

    add mutate_all_remove_multibyte_chars function

---
+++ R/mutate_all_remove_multibyte_chars.R
@@ -0,0 +1,12 @@
+#' Remove all multibyte characters in the dataframe
+#' @importFrom dplyr mutate_all
+#' @importFrom stringr str_replace_all
+#' @export
+
+mutate_all_rm_multibyte_chars <-
+    function(dataframe) {
+        return(
+            dataframe %>%
+           dplyr::mutate_all(stringr::str_replace_all, "[^ -~]", ""))
+        
+    }
