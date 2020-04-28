b541c2b294332fe342eebeb86f4f825570341fc3
Meera <patelm9@mskcc.org>
2020-04-27 22:40:58 -0400

    add cleanup_listnames function

---
+++ R/cleanup_listnames.R
@@ -0,0 +1,21 @@
+#' Standardize List Names
+#' This function takes a dataframe and changes column names in the following order: 1) Trims whitespace on both sides, 2) converts to uppercase, 3) replaces punctuation of any length with a single underscore, 4) removes trailing underscores from native column names that often times have trailing punctuation (usually decimals)
+#' @importFrom purrr map
+#' @importFrom centipede trimws
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_remove_all
+#' @export
+
+cleanup_listnames <-
+        function(list) {
+                names(list) <-
+                        names(list) %>%
+                                purrr::map(centipede::trimws, "both") %>%
+                                purrr::map(toupper) %>%
+                                purrr::map(stringr::str_replace_all, "[[:punct:]]{1,}", "_") %>%
+                                purrr::map(stringr::str_remove_all, "[_]{1}$") %>%
+                                purrr::map(stringr::str_replace_all, " {1,}", "_")
+                names(list) <- gsub("[_]{2,}", "_", names(list))
+
+                return(list)
+        }
---
+++ README.md
@@ -0,0 +1,10 @@
+2020-04-27 16:23:51 	File Created
+
+# Typewrite Messages
+Timestamp:	2020-04-27 22:40:57
+Local R Script:	~/GitHub/Public-Packages/rubix/R/cleanup_listnames.R
+Typewrite Message:	No deleted files in this repo.
+Timestamp:	2020-04-27 16:23:51
+Local R Script:	~/GitHub/Public-Packages/rubix/R/filter_by_words_in_str_before_zero.R
+Typewrite Message:	No deleted files in this repo.
+
