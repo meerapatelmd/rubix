7b583bba197d24b14cae11698216d665b50d8f02
Meera <patelm9@mskcc.org>
2020-05-02 15:31:06 -0400

    add map_names_set function

---
+++ R/map_names_set.R
@@ -0,0 +1,19 @@
+#' Map and set_names function from purrr package in tandem
+#' @description This function automatically sets the names in the list or atomic vector from the input data without having to make 2 separate function calls to purrr::map and purrr::set_names.
+#' @param x input
+#' @param .f function in purrr::map
+#' @param ... additional parameters for the purrr::map function
+#' @export
+
+map_names_set <-
+        function(x, .f, ...) {
+                if (is.list(x)) {
+                        x %>%
+                                purrr::set_names(names(x)) %>%
+                                purrr::map(.f,...)
+                } else {
+                        x %>%
+                                purrr::set_names(as.character(x)) %>%
+                                purrr::map(.f,...)
+                }
+        }
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 2020-04-27 16:23:51 	File Created
 
 # Typewrite Messages
+Timestamp:	2020-05-02 15:31:06
+Local R Script:	~/GitHub/Public-Packages/rubix/R/map_names_set.R
+Typewrite Message:	No deleted files in this repo.
+Timestamp:	2020-05-02 14:46:58
+Local R Script:	~/GitHub/Public-Packages/rubix/DESCRIPTION
+Typewrite Message:	No deleted files in this repo.
+Timestamp:	2020-05-02 14:46:58
+Local R Script:	~/GitHub/Public-Packages/rubix/DESCRIPTION
+Typewrite Message:	No untracked files in this repo.
+
 Timestamp:	2020-05-02 14:46:32
 Local R Script:	~/GitHub/Public-Packages/rubix/DESCRIPTION
 Typewrite Message:	No deleted files in this repo.
