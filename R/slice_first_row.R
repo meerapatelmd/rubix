032182321fe0f0fad4373a3e59c0fe629495e2a3
Meera <patelm9@mskcc.org>
2020-05-10 16:07:04 -0400

    add slice_first_row function

---
+++ R/slice_first_row.R
@@ -0,0 +1,9 @@
+#' Slice first row
+#' @importFrom dplyr slice
+#' @export
+
+slice_first_row <-
+        function(dataframe) {
+                dataframe %>%
+                        dplyr::slice(-1)
+        }
