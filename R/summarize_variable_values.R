04ba739a9c5e99389038e2a9008fa195a07d404a
Meera <patelm9@mskcc.org>
2020-05-22 00:43:39 -0400

    bug fix summarize_variable_metric function

--- R/summarize_variable_values.R
+++ R/summarize_variable_values.R
@@ -58,7 +58,7 @@ summarize_variable_values <-
                 output_2 <- 
                         output_1 %>%
                         tidyr::pivot_longer(cols = everything(),
-                                            names_pattern = "(^.*?)[_](.*$)",
+                                            names_pattern = "(^.*?)[_](VALUES)",
                                             names_to = c("Variable", "Metric Type"),
                                             values_to = "Metric") %>%
                         tidyr::pivot_wider(id_cols = Variable,
