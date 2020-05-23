edd1325ddc1c8cf06479796a70a0e2d2982f6e38
Meera <patelm9@mskcc.org>
2020-05-22 20:37:24 -0400

    bug fix mutate1 function

--- R/mutate1.R
+++ R/mutate1.R
@@ -8,13 +8,10 @@
 mutate1 <-
         function(dataframe, ...) {
                 col <- list(...)
-                for (i in 1:length(col)) {
-                        dataframe <-
-                                dataframe %>%
-                                dplyr::mutate(`:=`(!!names(col)[i], col[[i]])) 
-                                
-                                
-                }
+                
+                dataframe <- 
+                        dataframe %>%
+                        dplyr::mutate(...)
 
                  dataframe %>%
                          dplyr::select(!!names(col), everything())
