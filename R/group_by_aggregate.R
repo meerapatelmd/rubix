85aa496b849fd9d22c9153bd6bd174406beb1d94
Meera <patelm9@mskcc.org>
2020-05-23 00:13:43 -0400

    add group_by_aggregate function

---
+++ R/group_by_aggregate.R
@@ -0,0 +1,27 @@
+#' Aggregate Function
+#' @importFrom dplyr enquos
+#' @importFrom dplyr enquo
+#' @importFrom dplyr group_by
+#' @importFrom dplyr summarize_at
+#' @importFrom dplyr ungroup
+#' @export
+
+group_by_aggregate <- 
+                function(dataframe, 
+                         ...,
+                         agg.col,
+                         collapse = "|") {
+                        
+                                
+                                        group_by_cols <- dplyr::enquos(...) 
+                                        agg.col <- dplyr::enquo(agg.col)
+                                        
+                                        
+                                        dataframe %>%
+                                                dplyr::group_by(!!!group_by_cols) %>%
+                                                dplyr::summarize_at(vars(!!agg.col), function(x) paste0(x, collapse = collapse)) %>%
+                                                dplyr::ungroup()
+                
+                
+                
+                }
