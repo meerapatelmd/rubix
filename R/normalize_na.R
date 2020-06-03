e04174a2cb6eb4eb980c84ff36e2f9722fc830f3
Meera <patelm9@mskcc.org>
2020-06-03 12:29:35 -0400

    add normalize_na function

---
+++ R/normalize_na.R
@@ -0,0 +1,13 @@
+#' Normalize NA values
+#' @description convert all "NA" and true NAs to NA
+#' @importFrom stringr str_replace_All
+#' @importFrom dplyr mutate_all
+#' @importFrom dplyr na_if
+#' @export
+
+normalize_na <-
+        function(.data) {
+                .data %>%
+                        dplyr::mutate_all(stringr::str_replace_all, "^NA$", "") %>%
+                        dplyr::mutate_all(dplyr::na_if, "")
+        }
