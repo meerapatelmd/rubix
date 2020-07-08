c2de991299225e4f394c139c29001c2b888a2aab
Meera <patelm9@mskcc.org>
2020-07-08 18:17:19 -0400

    bug fix filter_for function

--- R/filter_for.R
+++ R/filter_for.R
@@ -1,11 +1,12 @@
-#' Filter for observations using a character vector of included values
+#' Filter a column for a set of values
+#' @description This is a shortcut to using the dplyr::filter({col} %in% {vector}) function along with its inverse.
 #' @param filter_col column targeted for filtering
 #' @param inclusion_vector a vector of values to be included
 #' @importFrom magrittr %>%
 #' @importFrom dplyr filter_at
 #' @export
 
-filter_for_vector <-
+filter_for <-
         function(dataframe, filter_col, inclusion_vector, invert = FALSE) {
                 filter_col <- enquo(filter_col)
                 
