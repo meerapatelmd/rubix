13d82a1912980d475703bfd4d2a9c8e72c81c4b6
Meera <patelm9@mskcc.org>
2020-07-08 18:02:00 -0400

    add normalize_all_to_na function

---
+++ R/normalize_all_to_na.R
@@ -0,0 +1,22 @@
+#' Normalize a set of values to a true NA
+#' @desciption This functions takes all the fields in a dataframe and replaces the "NA" string with NA_character_. If blanks is set to TRUE, blanks of character length of 0 are also replaced with NA_character_.
+#' @import dplyr
+#' @import stringr
+#' @export
+
+
+
+normalize_all_to_na <- 
+        function(.data, blanks = TRUE, ...) {
+                .data <-
+                .data %>%
+                        dplyr::mutate_all(stringr::str_replace_all, "^NA$", NA_character_) 
+                
+                if (blanks) {
+                        
+                        .data <- 
+                                .data %>%
+                                dplyr::mutate_all(stringr::str_replace_all, "^$", NA_character_)
+                        
+                }
+        }
