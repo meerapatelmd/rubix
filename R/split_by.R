0452fc8702c6dd78855471925d921b93dce62a8a
Meera <patelm9@mskcc.org>
2020-07-24 19:39:07 -0400

    bug fix split functions

--- R/split_by.R
+++ R/split_by.R
@@ -1,11 +1,12 @@
 #' Split a dataframe into a list with the column removed
 #' @importFrom dplyr select
+#' @importFrom dplyr enquo
 #' @export
 
 
 split_by <-
         function(x, column) {
-                column <- enquo(column)
+                column <- dplyr::enquo(column)
                 split(x, x %>%
                               dplyr::select(!!column) %>%
                               unlist() %>%
--- R/split_deselect.R
+++ R/split_deselect.R
@@ -1,12 +1,13 @@
 #' Split a dataframe into a list with the column removed
 #' @importFrom dplyr select
+#' @importFrom dplyr enquo
 #' @export
 
 
 split_deselect <-
         function(x, column) {
                 
-                column <- enquo(column)
+                column <- dplyr::enquo(column)
                 output <- 
                         split_by(x = x,
                                  column = !!column)
