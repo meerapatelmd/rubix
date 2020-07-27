cf9ed99c9abb9775d6275533109a5143c550094b
Meera <patelm9@mskcc.org>
2020-07-27 17:33:16 -0400

    add coalesce functions

--- R/coalesce_into.R
+++ R/coalesce_into.R
@@ -1,9 +1,10 @@
 #' Coalesce 1+ Fields to a New Field
+#' @description This is 1 of 2 coalesce functions. This one takes fields that are not the same as the proposed new column name. For example, a dataframe with fields "A", "B", and "C" can be coalesced into a field "D". If all the values need to be coalesced into an existing field such as "A", "B", and "C" to "A", use coalesce_at function instead.
 #' @import dplyr
 #' @export
 
 
-coalesce_to <- 
+coalesce_into <- 
         function(.data,
                  into,
                  ...,
