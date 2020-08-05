7f08ff701ec1beeafcb2f4dc4ade878afc772a17
Meera Patel <patelmeeray@gmail.com>
2020-08-05 00:16:46 -0400

    add filterByList function

---
+++ R/filterByList.R
@@ -0,0 +1,31 @@
+#' Filter By a List and Other Filters
+#' @import purrr
+#' @import rlang
+#' @import secretary
+#' @export
+
+filterByList <- 
+        function(.data, ...) {
+                
+                Settings <- rlang::list2(...)
+                
+                settings <-
+                        Settings %>%
+                        purrr::map_if(function(x) !(length(x) == 1 && is.na(x)),
+                                      function(x) cave::vector_to_string(x)) %>%
+                        purrr::map_if(function(x) (length(x) == 1 && is.na(x)),
+                                      function(x) paste0("c(", x, ")")) %>%
+                        purrr::map2(names(Settings),
+                                    function(x,y) paste("\t", y,"%in%", x, collapse = " ")) %>%
+                        unlist() %>%
+                        unname() %>%
+                        paste(collapse = ",\n")
+                
+                secretary::typewrite_bold("Settings:")
+                secretary::typewrite(settings)
+                
+                cat("\n")
+                
+                eval(rlang::parse_expr(paste0(".data %>% dplyr::filter(", settings, ")")))
+                
+        }
