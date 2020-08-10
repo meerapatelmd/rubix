e9ebcfbc0ce5cc33e2ea4584bec85b758382bf2d
Meera <patelm9@mskcc.org>
2020-08-10 15:56:01 -0400

    add filterList function

--- R/filterByList.R
+++ R/filterByList.R
@@ -1,9 +1,12 @@
 #' Filter By a List and Other Filters
 #' @description A List object with name as filter fields and values can be used by splicing it with !!! when included as an argument.
-#' @import purrr
-#' @import rlang
-#' @import secretary
-#' @export
+#' @rdname filterByList
+#' @export 
+#' @importFrom rlang list2 parse_expr
+#' @importFrom purrr map_if map2
+#' @importFrom cave vector_to_string
+#' @importFrom secretary typewrite_bold typewrite
+
 
 filterByList <- 
         function(.data, ...) {
