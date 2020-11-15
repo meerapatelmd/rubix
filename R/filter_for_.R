b0322e146d74a946a99932cd5461e3f863724be1
Meera Patel <patelmeeray@gmail.com>
2020-11-15 01:02:58 -0500

    add filter_for function

--- R/filter_for_.R
+++ R/filter_for_.R
@@ -59,3 +59,33 @@ filter_for_all <-
                 }
                 
         }
+
+
+#' Filter a column for a set of values
+#' @description This is a shortcut to using the dplyr::filter({col} %in% {vector}) function along with its inverse.
+#' @param cols column targeted for filtering
+#' @param vector a vector of values to be included
+#' @importFrom magrittr %>%
+#' @importFrom dplyr filter_at
+#' @export
+
+filter_for <-
+        function(data, 
+                 col, 
+                 vector, 
+                 invert = FALSE) {
+                
+
+                if (invert) {
+                        
+                        data %>%
+                                dplyr::filter_at(dplyr::vars({{ col }}), dplyr::all_vars(!(. %in% vector)))
+                        
+                        
+                } else {
+                        
+                        data %>%
+                                dplyr::filter_at(dplyr::vars({{ col }}), dplyr::all_vars(. %in% vector))
+                }
+                
+        }
