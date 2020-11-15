ff264b936fdf0ee4786fc104f3b0e2dce471b376
Meera Patel <patelmeeray@gmail.com>
2020-11-15 01:19:25 -0500

    update coalesce_ docs

--- R/coalesce_.R
+++ R/coalesce_.R
@@ -1,16 +1,21 @@
 #' Coalesce 1+ Fields to an Existing Field
-#' @description This is 2 of 2 coalesce functions. 
-#' @param at_var character vector of length 1 of the column that the coalesce function will be combine to
+#' @description 
+#' Coalesce a set of fields to an existing column `col` in the order in which the arguments are supplied with `col` leading.
+#' 
+#' @inheritParams wrapper_args
+#' @param ... Other columns in addition to `col` that will be coalesced in the correct order of precedence. 
+#' @param remove Should columns supplied in `...` be removed in the output?
 #' @seealso 
-#'  \code{\link[dplyr]{tidyeval-compat}},\code{\link[dplyr]{mutate_all}},\code{\link[dplyr]{select}},\code{\link[dplyr]{mutate}}
+#'  \code{\link[dplyr]{tidyeval-compat}},\code{\link[dplyr]{select}},\code{\link[dplyr]{mutate_all}},\code{\link[dplyr]{vars}},\code{\link[dplyr]{mutate}},\code{\link[dplyr]{coalesce}},\code{\link[dplyr]{select_all}}
 #' @rdname coalesce_at
-#' @export 
-#' @importFrom dplyr enquos mutate_at select mutate %>%
+#' @export
+#' @importFrom dplyr enquos select mutate_at vars mutate coalesce select_at
+
 
 
 coalesce_at <- 
         function(data,
-                 at_var,
+                 col,
                  ...,
                  remove = TRUE) {
 
@@ -25,18 +30,18 @@ coalesce_at <-
                                 colnames()
                  
                         data %>%
-                                dplyr::mutate_at(vars({{ at_var }}, !!!cols),
+                                dplyr::mutate_at(dplyr::vars({{ col }}, !!!cols),
                                                  as.character) %>%
-                                dplyr::mutate({{ at_var }} := dplyr::coalesce({{ at_var }}, !!!cols)) %>%
-                                dplyr::select_at(vars(!all_of(rm_cols_vector)))
+                                dplyr::mutate({{ col }} := dplyr::coalesce({{ col }}, !!!cols)) %>%
+                                dplyr::select_at(dplyr::vars(!all_of(rm_cols_vector)))
                         
                         
                 } else {
                         
                         data %>%
-                                dplyr::mutate_at(vars({{ at_var }}, !!!cols),
+                                dplyr::mutate_at(dplyr::vars({{ col }}, !!!cols),
                                                  as.character) %>%
-                                dplyr::mutate({{ at_var }} := dplyr::coalesce({{ at_var}},
+                                dplyr::mutate({{ col }} := dplyr::coalesce({{ col}},
                                                                               !!!cols))
                 }
 
@@ -49,14 +54,18 @@ coalesce_at <-
 
 #' @title
 #' Coalesce 1+ Fields to a New Field
+#' 
 #' @description 
 #' (Deprecated) This is 1 of 2 coalesce functions. This one takes fields that are not the same as the proposed new column name. For example, a dataframe with fields "A", "B", and "C" can be coalesced into a field "D". If all the values need to be coalesced into an existing field such as "A", "B", and "C" to "A", use coalesce_at function instead.
+#' 
+#' @inheritParams coalesce_at
+#' 
 #' @seealso 
 #'  \code{\link[dplyr]{tidyeval-compat}},\code{\link[dplyr]{select}},\code{\link[dplyr]{mutate}},\code{\link[dplyr]{coalesce}}
-#' @rdname coalesce_into
+#'  
+#' @rdname coalesce_to
 #' @export 
-#' @importFrom dplyr enquos enquo select mutate coalesce %>%
-
+#' @importFrom dplyr vars enquos select mutate mutate_at coalesce select_at
 
 coalesce_to <- 
         function(data,
@@ -85,17 +94,17 @@ coalesce_to <-
 
                         data %>%
                                 dplyr::mutate({{ col }} := "") %>%
-                                dplyr::mutate_at(vars(!!!cols),
+                                dplyr::mutate_at(dplyr::vars(!!!cols),
                                                  as.character) %>%
                                 dplyr::mutate({{ col }} := dplyr::coalesce(!!!cols)) %>%
-                                dplyr::select_at(vars(!all_of(rm_cols_vector)))
+                                dplyr::select_at(dplyr::vars(!all_of(rm_cols_vector)))
 
 
                 } else {
 
                         data %>%
                                 dplyr::mutate({{ col }} := "") %>%
-                                dplyr::mutate_at(vars(!!!cols),
+                                dplyr::mutate_at(dplyr::vars(!!!cols),
                                                  as.character) %>%
                                 dplyr::mutate({{ col }} := dplyr::coalesce(!!!cols))
                 }
