fa3569c549df81e6640fbb36ae8c74d10317a3b5
Meera Patel <patelmeeray@gmail.com>
2020-11-15 21:24:17 -0500

    add forcats extension funs

---
+++ R/forcats.R
@@ -0,0 +1,42 @@
+
+
+
+#' @export
+
+categorize <-
+        function(data, col, ...) {
+
+                data %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), factor) %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), forcats::fct_collapse, ...) %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), as.character)
+                
+        }
+
+#' @export
+
+recode_value <- 
+        function(data, col, ...) {
+                
+                data %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), factor) %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), forcats::fct_recode, ...) %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), as.character)
+                
+        }
+
+#' @export
+
+recode_boolean <- 
+        function(data, col, true_value, false_value) {
+                
+                data %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), factor) %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), forcats::fct_collapse, `TRUE` = true_value, `FALSE` = false_value) %>%
+                        dplyr::mutate_at(dplyr::vars({{ col }}), as.logical)
+                
+        }
+
+
+
+
