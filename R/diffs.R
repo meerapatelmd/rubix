355cf7e9be8a3d8dc2276549f74a37abbb5d00e2
Meera Patel <patelmeeray@gmail.com>
2020-11-17 17:25:45 -0500

    add diff examples

--- R/diffs.R
+++ R/diffs.R
@@ -1,56 +1,97 @@
-#' Get the number of rows that differ between 2 dataframes
-#' @importFrom dplyr setdiff
+#' @title 
+#' Diff Functions
+#' 
+#' @param x A dataframe or tibble.
+#' @param y A dataframe or tibble.
+#' 
+#' @name diffs
+NULL
+
+
+#' @title 
+#' Number of Different Rows between 2 Dataframes
+#' 
+#' @inheritParams diffs
+#' @rdname setdiff_nrow
+#' @family diff functions
 #' @export
+#' @example inst/examples/diffs.R
 
 setdiff_nrow <-
         function(x, y) {
                 
-                nrow(dplyr::setdiff(x, y))
+                nrow(setdiff(x, y))
                 
         }
 
+#' @title 
+#' Set Difference between Overlapping Columns
+#' @description 
+#' Preemptively select for columns that overlap between 2 dataframes before calling the `setdiff()` function.
+#' @inheritParams diffs
+#' @seealso 
+#'  \code{\link[purrr]{map}},\code{\link[purrr]{reduce}}
+#'  \code{\link[dplyr]{select}},\code{\link[dplyr]{reexports}}
+#' @rdname setdiff_col_match
+#' @family diff functions
+#' @export 
+#' @importFrom purrr map reduce
+#' @importFrom dplyr select all_of
+#' @example inst/examples/diffs.R
 
 setdiff_col_match <- 
-        function(data_a,
-                 data_b) {
+        function(x,
+                 y) {
                 
                 
-                cols <- colnames(data_a)[colnames(data_a) %in% colnames(data_b)]
+                cols <- colnames(x)[colnames(x) %in% colnames(y)]
                 
 
-                        list(A = data_a,
-                             B = data_b) %>%
+                list(X = x,
+                     Y = y) %>%
                         purrr::map(function(x) 
                                                 x %>% 
                                                 dplyr::select(dplyr::all_of(cols))) %>%
                         purrr::reduce(setdiff)
                 
                 
-                
         }
 
 
-
+#' @title 
+#' Compare Column Names and Positions
+#' @description 
+#' Compare column names and positions for 2 dataframes. 
+#' @inheritParams diffs
+#' @seealso 
+#'  \code{\link[purrr]{map}},\code{\link[purrr]{reduce}}
+#'  \code{\link[dplyr]{select}},\code{\link[dplyr]{reexports}}
+#' @rdname setdiff_col_match
+#' @family diff functions
+#' @export 
+#' @importFrom purrr map reduce
+#' @importFrom dplyr select all_of
+#' @example inst/examples/diffs.R
 
 compare_cols <- 
         function(
-          data_a,
-          data_b) {
+          x,
+          y) {
                 
                 
                 
 
-                a <- tibble::tibble(column_a = colnames(data_a)) %>%
-                        tibble::rowid_to_column(var = "position_a") %>%
-                        dplyr::mutate(column = column_a)
+                a <- tibble::tibble(column_x = colnames(x)) %>%
+                        tibble::rowid_to_column(var = "position_x") %>%
+                        dplyr::mutate(x_and_y_columns = column_x)
                 
-                b <- tibble::tibble(column_b= colnames(data_b)) %>%
-                        tibble::rowid_to_column(var = "position_b") %>%
-                        dplyr::mutate(column = column_b)
+                b <- tibble::tibble(column_y = colnames(y)) %>%
+                        tibble::rowid_to_column(var = "position_y") %>%
+                        dplyr::mutate(x_and_y_columns = column_y)
                 
                 dplyr::full_join(a, 
                                  b, 
-                                 by = "column")
+                                 by = "x_and_y_columns")
 
                 
         }
