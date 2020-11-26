fa3073a753a1c9d8bd12ac0be2282f251e30ccb9
Meera <patelm9@mskcc.org>
2020-11-25 22:55:04 -0500

    restore normalize_all_to_na.R for skyscraper package

---
+++ R/normalize_all_to_na.R
@@ -0,0 +1,22 @@
+#' Normalize a set of values to a true NA
+#' @description This functions takes all the fields in a dataframe and replaces the "NA" string with NA_character_. If blanks is set to TRUE, blanks of character length of 0 are also replaced with NA_character_.
+#' @import dplyr
+#' @import stringr
+#' @export
+
+
+
+normalize_all_to_na <- 
+        function(.data, blanks = TRUE) {
+                
+                .data <- mutate_all_na_str_to_na(.data = .data)
+
+                if (blanks) {
+                        
+                        .data <- 
+                                mutate_all_blank_to_na(.data = .data)
+                        
+                }
+                
+                return(.data)
+        }
